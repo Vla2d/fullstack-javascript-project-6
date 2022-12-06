d0e1593b78825d12a1450ab742130be49b198aa4
Vladyslav Melnyk <fireflay.2005@gmail.com>
2022-12-06 14:24:01 -0600

    emty getSessionCookie func

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -1,14 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const getSessionCookie = async (app) => {
-  await app.objection.knex('users').insert({ email: 'admin123@gmail.com', password_digest: 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3' });
-  const response = await app.inject({
-    method: 'POST',
-    url: '/session',
-    payload: {
-      data: { email: 'admin123@gmail.com', password: '123' },
-    },
-  });
-  const [sessionCookie] = response.cookies;
-  const { name, value } = sessionCookie;
-  return { [name]: value };
-};
+export const getSessionCookie = () => ({});
