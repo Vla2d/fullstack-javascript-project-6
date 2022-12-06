21ff3d6c57844f88766dd9f8339eba350bbbc392
Vladyslav Melnyk <fireflay.2005@gmail.com>
2022-12-06 14:30:40 -0600

    knex.insert restored

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -1,2 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
-export const getSessionCookie = async () => ({});
+export const getSessionCookie = async (app) => {
+  await app.objection.knex('users').insert({ email: 'admin123@gmail.com', password_digest: 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3' });
+
+  return {};
+};
