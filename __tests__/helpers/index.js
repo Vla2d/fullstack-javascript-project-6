a1d48c263a5d1cdec1339102938970e5e5c6abee
Vladyslav Melnyk <fireflay.2005@gmail.com>
2022-12-07 21:06:18 -0600

    try catch removed

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const getSessionCookie = async (app) => {
-  try {
+  // try {
     await app.objection.knex('users').insert({ email: 'admin123@gmail.com', password_digest: 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3' });
     const response = await app.inject({
       method: 'POST',
@@ -12,8 +12,8 @@ export const getSessionCookie = async (app) => {
     const [sessionCookie] = response.cookies;
     const { name, value } = sessionCookie;
     return { [name]: value };
-  } catch (e) {
-    console.log(e);
-    return {};
-  }
+  // } catch (e) {
+  //   console.log(e);
+  //   return {};
+  // }
 };
